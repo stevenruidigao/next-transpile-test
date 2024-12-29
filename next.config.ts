ca40dbc2664ea8ed2c944912a812b2048eeb1e35
Steven Gao <stevenruidigao@gmail.com>
2024-12-29 17:08:28 -0500

    add browserslist targets

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ['next', 'react', 'react-dom'],
+  webpack: (config) => {
+    config.optimization.minimize = false;
+    return config;
+  },
 };
 
 export default nextConfig;
